a3cd21c19713b570d212e3cb7f8b9b1b88f25a59
jines <ranawatjinesh@gmail.com>
2020-06-14 22:13:45 +0530

    Array List with out duplicates

--- src/com/study/hashcodeequals/FirstMain.java
+++ src/com/study/hashcodeequals/FirstMain.java
@@ -2,13 +2,20 @@ package com.study.hashcodeequals;
 
 public class FirstMain {
     public static  void  main(String[] args){
-        Student s1=new Student(150,"Jinesh");
-        Student s2=new Student(2,"Jinesh");
+        Student s1=new Student(1,"Jinesh");
+        Student s2=new Student(1,"Jinesh");
         Student s3= new Student(10,"Myra");
         //Even if student objects are same hashcode is different
         System.out.println("Hash code of s1 is"+s1.hashCode());
         System.out.println("hash code of s2 is"+s2.hashCode());
         System.out.println("Check 2 sutudent are same or not please find the boolean value for same "+s1.equals(s2));
+         /*
+        Output without hashcode and equals
+       Hash code of s1 is-2076061685
+       Hash code of s2 is-2076061685
+       Check 2 sutudent are same or not please find the boolean value for same true
+         */
+
     }
 
 }
---
+++ src/com/study/hashcodeequals/SecondMain.java
@@ -0,0 +1,49 @@
+package com.study.hashcodeequals;
+import java.util.*;
+public class SecondMain {
+    public static  void  main(String[] args){
+
+        Student s1=new Student(150,"Jinesh");
+        Student s2=new Student(2,"Jinesh");
+        Student s3= new Student(10,"Myra");
+
+
+
+
+        ArrayList<Student> data =new ArrayList<Student>();
+        data.add(s1);
+        data.add(s2) ;
+        data.add(new Student(122,"Jinesh"));
+        data.add(s3);
+        // Following object is repeated , and so we should see one less count
+        data.add(new Student(150,"Jinesh"));
+        HashSet<Student> dataSet=new HashSet<>();
+        dataSet.addAll(data);
+        ArrayList<Student> data1=new ArrayList<>(dataSet);
+
+        System.out.println("Check number of objects stored "+data1.size());
+        Collections.sort(data,new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o2.getId()-o1.getId();
+            }
+        });
+
+
+        for(Student list:data){
+            System.out.println(list.id+list.name);
+        }
+        System.out.println("Check number of objects stored "+data.size());
+        /*
+        Check number of objects stored 4
+        150Jinesh
+        150Jinesh
+        122Jinesh
+            10Myra
+        2Jinesh
+        Check number of objects stored 5
+
+         */
+    }
+}
+
